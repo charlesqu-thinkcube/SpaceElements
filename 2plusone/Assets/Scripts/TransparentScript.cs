d5a0823c15a697152b6d5247a2d5ca9d5e0faa93
nottabitha <leimonistabitha@gmail.com>
2018-05-31 19:41:58 +1000

    Work

---
+++ 2plusone/Assets/Scripts/TransparentScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transparency();
+	}
+
+	void Transparency()
+	{
+		//SpriteRenderer.color = gameObject.GetComponent<SpriteRenderer>();
+		//SpriteRenderer.color = new Color(1f,1f,1f,.5f); 
+
+		//Color tmp = gameObject.GetComponent<SpriteRenderer>().color;
+ 		//tmp.a = 0f;
+ 		//gameObject.GetComponent<SpriteRenderer>().color = tmp;
+	}
+}
